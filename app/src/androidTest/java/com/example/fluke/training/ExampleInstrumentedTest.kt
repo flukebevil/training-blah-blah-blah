cce322f830a308c81542d50fdd7d2705c2ded476
Fluke <flukebevil@gmail.com>
2018-02-06 16:21:07 +0700

    first commit

---
+++ app/src/androidTest/java/com/example/fluke/training/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.example.fluke.training
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.example.fluke.training_blah_blah_blah", appContext.packageName)
+    }
+}
